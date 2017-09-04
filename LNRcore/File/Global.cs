a830af9b789c8c439a0348b888617d23995fa1bc
Rayyuan Xu <ruiyuanxu_info@163.com>
2017-09-04 16:01:23 +0800

    更换工作电脑之前最后一次提交

--- LNRcore/File/Global.cs
+++ LNRcore/File/Global.cs
@@ -11,26 +11,28 @@ namespace Core
 	{
 		public static string global_cfg_folder()
 		{
-			
-			return "";
+            var core_cfg = find_cfg();
+            return core_cfg.DirectoryName;
 		}
 
         // Todo: after getting my new laptop back, 
         // I'll check it out if it's my computer problem that I cannot use 
         // "System_Environment_GetFolderPath_System_Environment_SpecialFolder"
 
+        ////////////*** !!!!!!!CHanges needed/////////////
         /// <summary>
         /// gets the location of "Adddata"
         /// </summary>
         /// <returns></returns>
 		public static string global_appdata()
         {
-            return Environment.GetEnvironmentVariable("APPDATA").Replace(@"\",@"\\");
+            ///bullshit  I still have .Net Core1.0 on this computer
+            ///fuck this shit!!!!!
         }
 
 		public static FileInfo global_core_cfg()
 		{
-			throw new NotImplementedException();
+            return find_cfg();
 		}
 
 		private static FileInfo find_cfg()
@@ -38,29 +40,43 @@ namespace Core
             string p = "core.cnf";
 			if (File.Exists(p))
 				return new FileInfo("core.cnf");
-            if (File.Exists(p = global_appdata() + @"\core.cnf"))
+            if (File.Exists(p = global_appdata() + @"\LightNovelReader\core.cnf"))
                 return new FileInfo(p);
+            Directory.CreateDirectory(global_appdata() + @"\LightNovelReader");
             var nfs = File.Create(p);
-            var ifs = new StreamWriter(nfs);
+            var ifs = new StreamWriter(nfs, Encoding.UTF8);
+            Defaults.set_default_core_cnf(ifs);
             FileInfo newCfg = new FileInfo(p);
-            ifs.WriteLine("cfg_path={0}", newCfg.FullName);
-            // ... here should be more user preferences
+            //ifs.WriteLine("cfg_path ={0}", newCfg.FullName);
+            //// ... here should be more user preferences
             return newCfg;
 		}
 	}
 
-    public class Runtime
+    public static class Runtime
     {
-        private static Runtime _instance = null;
-        private Runtime() { }
-        public static Runtime GetInstance()
+        public static int LineWidth;
+
+
+        static Runtime()
         {
-            if (_instance == null)
-            {
-                _instance = new Runtime();
-            }
-            return _instance;
+            var gcnf = Global.global_core_cfg();
         }
     }
+
+    public static class Defaults
+    {
+        public static void set_default_core_cnf(StreamWriter fs)
+        {
+            fs.WriteLine("haha");
+        }
+
+        public static void set_default_book_cnf(StreamWriter fs)
+        {
+
+        }
+
+    }
+
 
 }
