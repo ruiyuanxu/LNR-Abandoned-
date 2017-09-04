89149ad887ef7b2444ee1289dfd3b3f395a16f13
徐瑞元 <mizumoto@vip.qq.com>
2017-09-04 21:43:38 +0800

    A total rewrite on Config settings

--- LNRcore/Core/Global.cs
+++ LNRcore/Core/Global.cs
@@ -7,52 +7,10 @@ using System.IO;
 
 namespace Core
 {
-	public static class Global
-	{
-		public static string global_cfg_folder()
-		{
-            var core_cfg = find_cfg();
-            return core_cfg.DirectoryName;
-		}
-
-        // Todo: after getting my new laptop back, 
-        // I'll check it out if it's my computer problem that I cannot use 
-        // "System_Environment_GetFolderPath_System_Environment_SpecialFolder"
-
-        ////////////*** !!!!!!!CHanges needed/////////////
-        /// <summary>
-        /// gets the location of "Adddata"
-        /// </summary>
-        /// <returns></returns>
-		public static string global_appdata()
-        {
-            ///bullshit  I still have .Net Core1.0 on this computer
-            ///fuck this shit!!!!!
-            string dest = Environment.
-        }
-
-		public static FileInfo global_core_cfg()
-		{
-            return find_cfg();
-		}
-
-		private static FileInfo find_cfg()
-		{
-            string p = "core.cnf";
-			if (File.Exists(p))
-				return new FileInfo("core.cnf");
-            if (File.Exists(p = global_appdata() + @"\LightNovelReader\core.cnf"))
-                return new FileInfo(p);
-            Directory.CreateDirectory(global_appdata() + @"\LightNovelReader");
-            var nfs = File.Create(p);
-            var ifs = new StreamWriter(nfs, Encoding.UTF8);
-            Defaults.set_default_core_cnf(ifs);
-            FileInfo newCfg = new FileInfo(p);
-            //ifs.WriteLine("cfg_path ={0}", newCfg.FullName);
-            //// ... here should be more user preferences
-            return newCfg;
-		}
-	}
+    public static class Global
+    {
+        
+    }
 
     public static class Runtime
     {
@@ -61,18 +19,18 @@ namespace Core
 
         static Runtime()
         {
-            var gcnf = Global.global_core_cfg();
+
         }
     }
 
     public static class Defaults
     {
-        public static void set_default_core_cnf(StreamWriter fs)
+        public static void set_default_core_cnf()
         {
-            fs.WriteLine("haha");
+            
         }
 
-        public static void set_default_book_cnf(StreamWriter fs)
+        public static void set_default_book_cnf()
         {
 
         }
--- LNRcore/Core/txt.cs
+++ LNRcore/Core/txt.cs
@@ -7,38 +7,38 @@ using System.Threading.Tasks;
 
 namespace Core
 {
-	public class txtBook : bfile, IBook
-	{
-		public override string hashname()
-		{
-			throw new NotImplementedException();
-		}
+    public class txtBook : bfile, IBook
+    {
+        public override string hashname()
+        {
+            throw new NotImplementedException();
+        }
 
-		public override void createcfg()
-		{
-			throw new NotImplementedException();
-		}
+        public override void createcfg()
+        {
+            throw new NotImplementedException();
+        }
 
-		public override void update()
-		{
-			throw new NotImplementedException();
-		}
+        public override void update()
+        {
+            throw new NotImplementedException();
+        }
 
-		public override string fetchLine(FileStream fs, int tag, out int length)
-		{
-			throw new NotImplementedException();
-		}
+        public override string fetchLine(FileStream fs, int tag, out int length)
+        {
+            throw new NotImplementedException();
+        }
 
-		public txtBook(): base() { }
+        public txtBook() : base() { }
 
-		public txtBook(FileInfo fi)
-		{
-			path = fi.FullName;
-			size = fi.Length;
-			bname = fi.Name;
-			hash = hashname();
-			type = fileType.txt;
-			cfgpath = Global.global_cfg_folder() + hash + ".cfg";
-		}
-	}
+        public txtBook(FileInfo fi)
+        {
+            path = fi.FullName;
+            size = fi.Length;
+            bname = fi.Name;
+            hash = hashname();
+            type = fileType.txt;
+            cfgpath = Global.global_cfg_folder() + hash + ".cfg";
+        }
+    }
 }
