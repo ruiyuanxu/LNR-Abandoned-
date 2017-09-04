6a184599b3d4b887fe7f8c756c7169a02e433bab
徐瑞元 <mizumoto@vip.qq.com>
2017-09-04 22:09:44 +0800

    Cheatsheet

---
+++ LNRCore/Core/Global.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Core
+{
+    public static class Global
+    {
+        
+    }
+
+    public static class Runtime
+    {
+        public static int LineWidth;
+        
+
+        static Runtime()
+        {
+
+        }
+    }
+
+    public static class Defaults
+    {
+        public static void set_default_core_cnf()
+        {
+            
+        }
+
+        public static void set_default_book_cnf()
+        {
+
+        }
+
+    }
+
+
+}
