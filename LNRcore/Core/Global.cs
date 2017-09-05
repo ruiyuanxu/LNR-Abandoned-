d68f2989435304f8db8ac7a1ccbe2aa0368b08bf
徐瑞元 <mizumoto@vip.qq.com>
2017-09-05 23:00:08 +0800

    Re-written the configuration part:)

--- LNRcore/Core/Global.cs
+++ LNRcore/Core/Global.cs
@@ -4,38 +4,78 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Windows.Storage;
 
 namespace Core
 {
-    public static class Global
+    /// <summary>
+    /// A enum that contains all names of the configuration
+    /// </summary>
+    public enum ConfigNames
     {
-        
+        WrapWidth                   = 0,
+        ParaLength                  = 1,
+        Font                        = 2,
+        ForeGroundWidthPercentage   = 3,
+        Themes                      = 4
     }
-
-    public static class Runtime
+    
+    public static class Config
     {
-        public static int LineWidth;
-
-
-        static Runtime()
+        /// <summary>
+        /// Returns Container of the settings
+        /// </summary>
+        /// <returns>Configuration Collection</returns>
+        public static ApplicationDataContainer GlobalSettings()
         {
-
-        }
-    }
-
-    public static class Defaults
-    {
-        public static void set_default_core_cnf()
-        {
-            
-        }
-
-        public static void set_default_book_cnf()
-        {
-
+            return ApplicationData.Current.LocalSettings;
         }
 
+        public static void SetConfig(ConfigNames cname, object value)
+        {
+            string scn = Enum.GetName(typeof(ConfigNames), cname);
+            GlobalSettings().Values[scn] = value;
+        }
+
+        public static object GetConfig(ConfigNames cname)
+        {
+            return GlobalSettings().Values[Enum.GetName(typeof(ConfigNames), cname)];
+        }
+
+        public static void reset()
+        {
+            SetConfig(ConfigNames.WrapWidth, 40);
+            SetConfig(ConfigNames.ParaLength, 13 * 40);
+            SetConfig(ConfigNames.ForeGroundWidthPercentage, 0.618);
+            Themes.reset();
+        }
+
     }
 
+    /// <summary>
+    /// A class contains all theme configs.
+    ///                                 ->  theme0  ->backColor,foreColor...
+    ///                                 ->  theme1
+    /// Themes  ->  themes(composite)   ->  theme2
+    ///                                     ...
+    /// </summary>
+    public static class Themes
+    {
+        public static ApplicationDataCompositeValue ThemesContainer()
+        {
+            return (ApplicationDataCompositeValue)Config.GetConfig(ConfigNames.Themes);
+        }
+
+        public static void reset()
+        {
+            var themes = new ApplicationDataCompositeValue();
+            var themeDefaultLight = new ApplicationDataCompositeValue();
+            themeDefaultLight["backColor"] = "#C7EDCC";
+            themeDefaultLight["foreColor"] = "#EAF4FC"; //"月白"
+            themes["default-light"] = themeDefaultLight;
+            Config.GlobalSettings().Values["Themes"] = themes;
+        }
+
+    }
 
 }
